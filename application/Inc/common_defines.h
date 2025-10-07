e3d2886bfbf20ce7886ab4a644fc3539e621c611
Invictus Cockpit Systems <152350895+invictuscockpits@users.noreply.github.com>
2025-10-06 19:38:45 -0700

    Rearranged force anchor and config addresses

--- application/Inc/common_defines.h
+++ application/Inc/common_defines.h
@@ -14,7 +14,7 @@
   * under the terms of the GNU General Public License v3.0 or later:
   * https://www.gnu.org/licenses/gpl-3.0.html
   *
-  * Modifications and additions are © 2025 Invictus Cockpit Systems.
+  * Modifications and additions are ï¿½ 2025 Invictus Cockpit Systems.
   *
   * This software has been carefully modified for a specific purpose.  It is not recommended for use outside of the Invictus HOTAS system.
   *
@@ -34,7 +34,7 @@
 
 //#define DEBUG
 
-#define FIRMWARE_VERSION					0x2132    // v2.1.3 (Modifying I2C for communicating with Gen 4 Boards)
+#define FIRMWARE_VERSION					0x2142    // v2.1.34 (Modifying I2C for communicating with Gen 4 Boards) Also movedconfig address
 #define USED_PINS_NUM							30				// Contstant for HOTAS Control boards
 #define MAX_AXIS_NUM							8					// max 8
 #define MAX_BUTTONS_NUM						128				// power of 2, max 128
@@ -48,9 +48,9 @@
 #define AXIS_CENTER_VALUE					(AXIS_MIN_VALUE + (AXIS_MAX_VALUE-AXIS_MIN_VALUE)/2)
 #define AXIS_FULLSCALE						(AXIS_MAX_VALUE - AXIS_MIN_VALUE + 1)
 
-#define CONFIG_ADDR								(0x0800FC00)
+#define CONFIG_ADDR								(0x0800F400)
 #define FLASH_PAGE_SIZE           0x400
-#define FACTORY_ADDR              (CONFIG_ADDR - FLASH_PAGE_SIZE)  // protected page
+#define FACTORY_ADDR              (0x0800FC00)  // page 63 - protected from firmware flash
 #define FACTORY_MAGIC             0xF00C
 #define FACTORY_VERSION           0x02 //increase with major changes to protect older versions
 
